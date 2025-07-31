a79e87162727608d6c40239b9cccb4abe3cbdd2e
DenysHirniak <denforever777@gmail.com>
2025-07-31 16:29:10 +0300

    add method for random size of figure parameters

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,24 +6,25 @@ public class FigureSupplier {
     private static final ColorSupplier colorSupplier = new ColorSupplier();
     private static final Random rand = new Random();
 
+    private double randSize() {
+        return 1 + rand.nextDouble() * 24;
+    }
+
     public Figure getFigure() {
         int figureType = rand.nextInt(5);
         Color color = colorSupplier.getColor();
 
         switch (figureType) {
             case 0:
-                return new Circle(color, 1 + rand.nextDouble() * 24);
+                return new Circle(color, randSize());
             case 1:
-                return new Square(color, 1 + rand.nextDouble() * 24);
+                return new Square(color, randSize());
             case 2:
-                return new Rectangle(color, 1 + rand.nextDouble() * 24,
-                        1 + rand.nextDouble() * 24);
+                return new Rectangle(color, randSize(), randSize());
             case 3:
-                return new RightTriangle(color, 1 + rand.nextDouble() * 24,
-                        1 + rand.nextDouble() * 24);
+                return new RightTriangle(color, randSize(), randSize());
             case 4:
-                return new IsoscelesTrapezoid(color, 1 + rand.nextDouble() * 24,
-                        1 + rand.nextDouble() * 24, 1 + rand.nextDouble() * 24);
+                return new IsoscelesTrapezoid(color, randSize(), randSize(), randSize());
             default:
                 return getDefaultFigure();
         }
