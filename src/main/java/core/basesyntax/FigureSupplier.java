9ba238f08f75e74e4a3d1d40526a39d81d0c5724
DenysHirniak <denforever777@gmail.com>
2025-08-01 00:19:11 +0300

    split the Figure interface into two other interfaces

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -10,7 +10,7 @@ public class FigureSupplier {
         return 1 + rand.nextDouble() * 24;
     }
 
-    public Figure getFigure() {
+    public AbstractFigure getFigure() {
         int figureType = rand.nextInt(5);
         Color color = colorSupplier.getColor();
 
@@ -30,7 +30,7 @@ public class FigureSupplier {
         }
     }
 
-    public Figure getDefaultFigure() {
+    public AbstractFigure getDefaultFigure() {
         return new Circle(Color.WHITE, 10);
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
 
-        Figure[] figures = new Figure[6];
+        AbstractFigure[] figures = new AbstractFigure[6];
 
         for (int i = 0; i < 3; i++) {
             figures[i] = figureSupplier.getFigure();
@@ -14,7 +14,7 @@ public class Main {
             figures[i] = figureSupplier.getDefaultFigure();
         }
 
-        for (Figure figure : figures) {
+        for (AbstractFigure figure : figures) {
             figure.draw();
         }
 
