338a9f49f742fe5d851fbfc345115c9cd99e68ea
DenysHirniak <denforever777@gmail.com>
2025-08-07 14:40:03 +0300

    refactor main() method to use one loop for generating and drawing shapes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,16 +8,11 @@ public class Main {
 
         Figure[] figures = new Figure[ARRAY_OF_FIGURES_SIZE];
 
-        int halfSizeArray = ARRAY_OF_FIGURES_SIZE / 2;
-
-        for (int i = 0; i < ARRAY_OF_FIGURES_SIZE; i++) {
-            figures[i] = (i < halfSizeArray)
+        for (int i = 0; i < figures.length; i++) {
+            figures[i] = (i < figures.length / 2)
                     ? figureSupplier.getFigure()
                     : figureSupplier.getDefaultFigure();
-        }
-
-        for (Figure figure : figures) {
-            figure.draw();
+            figures[i].draw();
         }
     }
 }
